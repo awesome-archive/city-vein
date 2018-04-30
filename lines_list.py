b782d10bc40591ffc335a0b1ad656109214ca594
96486d9b <96486d9b@gmail.com>
2018-04-30 20:57:14 +0800

    crawl info of beijing and hangzhou, and visualize using baidu echarts

---
+++ lines_list.py
@@ -0,0 +1,87 @@
+from urllib import parse
+import hashlib
+import requests
+
+ak = '08eUG0hbUTzFrCFyF2Bn6tSQ7UD0cCaH'
+sk = '4Gzbk6HSzMHkWjjXliEOGM7ZAVvpqg0U'
+province = '浙江省'
+city = '杭州市'
+level = '公交站'
+
+
+def get_position(address):
+    queryStr = '/geocoder/v2/?address=%s&output=json&ak=%s' % (province + city + address + level, ak)
+    encodedStr = parse.quote(queryStr, safe="/:=&?#+!$,;'@()*[]")
+    rawStr = encodedStr + sk
+    sn = (hashlib.md5(parse.quote_plus(rawStr).encode("utf8")).hexdigest())
+    url = parse.quote("http://api.map.baidu.com" + queryStr + "&sn=" + sn, safe="/:=&?#+!$,;'@()*[]")
+    response = requests.get(url)
+    lng, lat = response.json()['result']['location']['lng'], response.json()['result']['location']['lat']
+    precise, confidence = response.json()['result']['precise'], response.json()['result']['confidence']
+    return lng, lat, precise, confidence
+
+
+# with open('./lines.json', "r") as f:
+#     lines = dict(eval(f.read()))
+#     for name, stations in lines.items():
+#         for station in stations:
+#             if station not in count.keys():
+#                 count[station] = 1
+#                 try:
+#                     geometry[station] = get_position(station)
+#                 except Exception as e:
+#                     print("[INFO] some error occur")
+#                     continue
+#             else:
+#                 count[station] += 1
+#         break
+#
+# count = sorted(count.items(),key = lambda x:x[1], reverse=True)
+# print(count)
+
+
+# df = pd.DataFrame(columns=['source', 'target'])
+# source = []
+# target = []
+
+# for line_name, line_stations in all_lines.items():
+#     print(line_name, line_stations)
+#     for i in range(len(line_stations)-1):
+#         source.append(get_position(line_stations[i]))
+#         target.append(get_position(line_stations[i+1]))
+#     break
+
+# df['source'] = source
+# df['target'] = target
+# df.to_csv('./data.csv', index=False)
+
+# print(get_position('古荡'))
+
+
+data = []
+
+with open('./lines.json', "r") as f:
+    lines = dict(eval(f.read()))
+    for name, stations in lines.items():
+        prev_lng, prev_lat = 0, 0
+        line = []
+        for station in stations:
+            try:
+                lng, lat, precise, confidence = get_position(station)
+                print(precise, confidence)
+                # if confidence <= 50:
+                #     continue
+            except Exception as e:
+                continue
+            if stations.index(station) == 0:
+                line.append(int(1e4 * lng))
+                line.append(int(1e4 * lat))
+                prev_lng, prev_lat = lng, lat
+            else:
+                line.append(int(1e4 * (lng - prev_lng)))
+                line.append(int(1e4 * (lat - prev_lat)))
+                prev_lng, prev_lat = lng, lat
+        data.append(line)
+        break
+
+print(data)
--- stations_list.py
+++ stations_list.py
@@ -12,7 +12,7 @@ links_letter = soup.find('div', class_='bus_kt_r2').find_all('a')
 links = links_number + links_letter
 # print(links)
 # 1, 2, 3, 4, B, D
-all_lines = {}
+all_lines = []
 for link in links:
     link_href = link['href']
     link_html = requests.get(url + link_href, headers=headers)
@@ -21,24 +21,25 @@ for link in links:
     for line in lines:
         line_href = line['href']
         line_name = line.get_text()
-        try:
-            line_html = requests.get(url + line_href, headers=headers)
-        except Exception as e:
-            print("[INFO] some error occur")
-            continue
-        line_info = {}
-        line_soup = bs4.BeautifulSoup(line_html.text, 'lxml')
-        bus_lines = line_soup.find_all('div', class_='bus_line_site')
-        for bus_line in bus_lines:
-            stations = []
-            bus_stations = bus_line.find_all('a')
-            for bus_station in bus_stations:
-                stations.append(bus_station.get_text())
-            if bus_lines.index(bus_line) == 0:
-                line_info[line_name + "-posi"] = stations
-            else:
-                line_info[line_name + "-nega"] = stations
-        all_lines.update(line_info)
+        # try:
+        #     line_html = requests.get(url + line_href, headers=headers)
+        # except Exception as e:
+        #     print("[INFO] some error occur")
+        #     continue
+        # line_info = {}
+        # line_soup = bs4.BeautifulSoup(line_html.text, 'lxml')
+        # bus_lines = line_soup.find_all('div', class_='bus_line_site')
+        # for bus_line in bus_lines:
+        #     stations = []
+        #     bus_stations = bus_line.find_all('a')
+        #     for bus_station in bus_stations:
+        #         stations.append(bus_station.get_text())
+        #     if bus_lines.index(bus_line) == 0:
+        #         line_info[line_name + "-posi"] = stations
+        #     else:
+        #         line_info[line_name + "-nega"] = stations
+        # all_lines.update(line_info)
+        all_lines.append(line_name)
         print("[INFO] get the info of line %s" % line_name)
     with open("./lines_beijing.json", "w") as f:
         f.write(str(all_lines))
